d0880afa47c8b1b0d8cd656af46e7302d49c42dd
iamhectorsosa <hello@hectorsosa.me>
2024-07-26 08:38:07 +0200

    feat: concurrency

---
+++ concurrency/check_websites_test.go
@@ -0,0 +1,32 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurtewe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurtewe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurtewe.geds":     false,
+	}
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
